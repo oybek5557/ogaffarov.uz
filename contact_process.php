026a9cbc60f83e1e3b886a4f3891609322a150b9
oybek5557 <gaffarovoybek21@gmail.com>
2020-01-12 16:45:11 +0500

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,17 +1,21 @@
 <?php
 
-    $to = "gaffarovoybek21@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+	$cmessage = $_REQUEST['message'];
+	
+	$headers['From']    = 'gaffarovoybek@yahoo.com';
+    $headers['To']      = 'gaffarovoybek21@gmail.com';
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    $body = 'Test message';
+
+    $smtpinfo["host"] = "smtp.mail.yahoo.com";
+    $smtpinfo["port"] = "465";
+    $smtpinfo["auth"] = true;
+    $smtpinfo["username"] = "gaffarovoybek@yahoo.com";
+    $smtpinfo["password"] = "babaq1234";
 
     $subject = "You have a message from your website Oybek Gaffarov.";
 
